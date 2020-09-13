8db89f6645e8d9bfd49acbdabf1c92116dfccc1d
Odd Stråbø <oddstr13@openshell.no>
2020-09-13 03:21:38 +0200

    Initial serde commit

---
+++ src/error.rs
@@ -0,0 +1,78 @@
+use std;
+use std::fmt::{self, Display};
+
+use serde::{de, ser};
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+// This is a bare-bones implementation. A real library would provide additional
+// information in its error type, for example the line and column at which the
+// error occurred, the byte offset into the input, or the current key being
+// processed.
+#[derive(Clone, Debug, PartialEq)]
+pub enum Error {
+	// One or more variants that can be created by data structures through the
+	// `ser::Error` and `de::Error` traits. For example the Serialize impl for
+	// Mutex<T> might return an error because the mutex is poisoned, or the
+	// Deserialize impl for a struct may return an error because a required
+	// field is missing.
+	Message(String),
+
+	// Zero or more variants that can be created directly by the Serializer and
+	// Deserializer without going through `ser::Error` and `de::Error`. These
+	// are specific to the format, in this case JSON.
+	Eof,
+	TooLarge,
+	Syntax,
+	ExpectedBoolean,
+	ExpectedInteger,
+	ExpectedString,
+	ExpectedNull,
+	ExpectedArray,
+	ExpectedMap,
+	ExpectedMapValue,
+	ExpectedSequenceEnd,
+	TrailingCharacters,
+
+
+	ExpectedEnum,
+}
+
+impl ser::Error for Error {
+	fn custom<T: Display>(msg: T) -> Self {
+		Error::Message(msg.to_string())
+	}
+}
+
+impl de::Error for Error {
+	fn custom<T: Display>(msg: T) -> Self {
+		Error::Message(msg.to_string())
+	}
+}
+
+
+impl Display for Error {
+	fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			Error::Message(msg) => formatter.write_str(msg),
+			Error::Eof => formatter.write_str("unexpected end of input"),
+			Error::TooLarge => formatter.write_str("input data too large"),
+			/* and so forth */
+			Error::Syntax => formatter.write_str("syntax error"),
+			Error::ExpectedBoolean => formatter.write_str("expected boolean"),
+			Error::ExpectedInteger => formatter.write_str("expected integer"),
+			Error::ExpectedString => formatter.write_str("expected string"),
+			Error::ExpectedNull => formatter.write_str("expected null"),
+			Error::ExpectedArray => formatter.write_str("expected array"),
+			Error::ExpectedMap => formatter.write_str("expected map"),
+			Error::ExpectedMapValue => formatter.write_str("expected map value"),
+			Error::ExpectedSequenceEnd => formatter.write_str("expected end of sequence"),
+			Error::TrailingCharacters => formatter.write_str("too much data"),
+
+		    Error::ExpectedEnum => formatter.write_str("expected enum"),
+		}
+	}
+}
+
+
+impl std::error::Error for Error {}
